eea419a8faeaf9428f629494d277e12a82521c86
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-08-01 01:28:58 +0530

    minor setting changes

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import settings
 
 urlpatterns = [
     path('', include('resume.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
