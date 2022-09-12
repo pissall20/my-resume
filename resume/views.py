dd8af5c0715483ac8a69dd96504402f66795b62e
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-09-12 18:13:09 +0530

    portfolio working, main ifframe not working

--- resume/views.py
+++ resume/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, JsonResponse
-from .forms import ContactForm
 from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
+
+from .forms import ContactForm
 from .models import Skill, Education, Experience, Portfolio, ProjectCategory
 
 
 # Create your views here.
+
+
+@xframe_options_exempt
+def ok_to_load_in_a_frame(request):
+    return HttpResponse("This page is safe to load in a frame on any site.")
+
+
 def index(request):
     contact_form = ContactForm()
     skills_list = Skill.objects.all().order_by("-rating")
     education = Education.objects.all()
     experience = Experience.objects.all()
-    portfolio = Portfolio.objects.all().order_by("order")
+    portfolios = Portfolio.objects.all().order_by("order")
     category = ProjectCategory.objects.all()
     skill_divider = int(len(skills_list) / 2)
     skills = {
@@ -23,10 +32,22 @@ def index(request):
         "skills": skills,
         "education": education,
         "experience": experience,
-        "portfolio": portfolio,
-        "categories":category
+        "portfolio": portfolios,
+        "categories": category
     }
     return render(request, 'resume/index.html', context)
+
+
+def portfolio(request, _id):
+    try:
+        data = Portfolio.objects.get(id=_id)
+    except Portfolio.DoesNotExist:
+        raise Http404('Data does not exist')
+
+    context = {
+        'portfolio': data
+    }
+    return render(request, 'resume/portfolio-details-view.html', context)
 
 
 def contact(request):
@@ -52,4 +73,3 @@ def contact(request):
 
     form = ContactForm()
     return render(request, 'aiprizm/index.html', {'form': form})
-
